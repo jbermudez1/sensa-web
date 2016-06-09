ad2d19d6c29f655399d71a46f019c3a9a3de26b0
diego <diego@proyeccione.com>
2016-06-08 19:15:24 -0600

    js y secciones

--- public/js/app.js
+++ public/js/app.js
@@ -59,12 +59,44 @@ function initMap() {
 	var sucursales = {
 		culiacanBravo: {
 			point: {lat:24.8018525, lng: -107.4023223},
-			details: 'Frente a la calle'
+			details: 'Nicolas Bravo #403 Nte. Col. Centro Tel: (667) 7131100 Mail: bravo@sensa.com.mx'
 		},
 		culiacanCentro: {
-			point: {lat:24.8281996, lng: -107.398024},
-			details: 'en el centro'
-		}
+			point: {lat:24.7963116, lng: -107.4139344},
+			details: 'Insurgentes #831 sur Tel: (667) 7147050 Mail: sensa@sensa.com.mx'
+		},
+    culiacanCedis: {
+      point: {lat:24.8139789, lng:-107.4612753},
+      details: 'Carretera a Culiacancito #8333-A Bella Vista, Tel: 9 92 16 51, 9 92 16 52, Mail: cedis@sensa.com.mx'
+    },
+    sonoraHermosillo: {
+      point: {lat:29.1153133, lng:-110.9533874},
+      details: 'Blvd. José María Morelos #267, Col.Loma Linda C.P.83150, Tel: (662)215 62 82, 2 10 72 93, 2 15 46 79, Mail: hermosillo@sensa.com.mx'
+    },
+    sonoraNogales: {
+      point: {lat:31.2974625, lng:-110.9554124},
+      details: 'Blvd. Luis Donaldo Colosio #4100 Bodega No.7, Col.Encinos, C.P.84064, Tel: 01(631) 319 24 16, Mail: nogales@sensa.com.mx'
+    },
+    bajacaliforniaEnsenada: {
+      point: {lat:31.862128, lng:-116.621885},
+      details: 'Av. Castillo #230, Zona Centro, C.P.22800, Tel: 01(646) 176 3540, Mail: ensenada@sensa.com.mx' 
+    },
+    bajacaliforniaMexicali: {
+      point: {lat:32.6251785, lng:-115.4238914},
+      details: 'Blvd. Lázaro Cárdenas #1377, Col. Independencia, C.P.21290, Tel: (686) 5655526 y 5676502, Mail: mexicali@sensa.com.mx'
+    },
+    bajacaliforniaTijuana: {
+      point: {lat:32.5120689, lng:-116.9955875},
+      details: 'Callejon de Carmen #3765. Col. 20 de noviembre, C.P 22430, Tel: (664) 6814028, Mail: tijuana@sensa.com.mx'
+    },
+    bajacaliforniasurLapaz: {
+      point: {lat:24.1345772, lng:-110.3155429},
+      details: 'Chiapas #3290-3 Col. Las garzas, C.P.23070, Tel: (612) 12 94 196, Mail: lapaz@sensa.com.mx'
+    },
+    jaliscoGuadalajara: {
+      point: {lat:20.6658391, lng:-103.3638809},
+      details: 'Av. Alemania #1694, Col. Moderna, C.P.44190 Ote, Tel: (333)8110310, Mail: guadalajara@sensa.com.mx'
+    },
 	}
 
 
